a8574b57244593cf0b2ae8fa8a3658cd8450e5cc
Ainiluth <136144828+Ainiluth@users.noreply.github.com>
2023-06-15 21:12:06 +0700

    Update Algo_latihan2.py
    Tugas formula matematika Algo_latihan.py2

--- Algo_latihan2.py
+++ Algo_latihan2.py
@@ -1,1 +1,42 @@
-maaf belum diisi..
+# Setting Variabel
+nama  = "Syifa Luthfia"
+kelas = "2A"
+prodi = "Informatika"
+tugas = "Algoritma Pemrograman"
+
+# Setting Variabel Nilai
+a = 10
+b = 20
+c = 50
+P = a
+L = b
+t = c
+
+# Rumus Formula
+forA = "((a+b)*(b*c))/(a+b+c)"
+forB = "P*L*t"
+forC = "(1/2)*L*t"
+forD = "(((a+b)^2)+((b*c)^2))/(a*b)"
+
+# Perhitungan
+mathA = ((a+b)*(b*c))/(a+b+c)
+mathB = P*L*t
+mathC = (1/2)*L*t
+mathD = (((a+b)^2)+((b*c)^2))/(a*b)
+
+# Cetak Hasil
+print("Nama  =",nama)
+print("Kelas =",kelas)
+print("Prodi =",prodi)
+print("Tugas =",tugas)
+print("------------------------------------------")
+print("         Hasil Formula Matematika         ")
+print("------------------------------------------")
+print("Hasil dari Operasi A  :",forA)
+print("                      =",mathA)
+print("Hasil dari Operasi B  :",forB)
+print("                      =",mathB)
+print("Hasil dari Operasi C  :",forC)
+print("                      =",mathC)
+print("Hasil dari Operasi D  :",forD)
+print("                      =",mathD)
