88fe42c459353b0c09636dcce0bb26ae2d043579
Ainiluth <136144828+Ainiluth@users.noreply.github.com>
2023-06-17 10:08:41 +0700

    Update Algo_latihan2.py

--- Algo_latihan2.py
+++ Algo_latihan2.py
@@ -12,17 +12,17 @@ P = a
 L = b
 t = c
 
-# Rumus Formula
+# Rumus 
 forA = "((a+b)*(b*c))/(a+b+c)"
 forB = "P*L*t"
-forC = "(1/2)*L*t"
-forD = "(((a+b)^2)+((b*c)^2))/(a*b)"
+forC = "1/2*a*t"
+forD = "(((a+b)**2)+((b*c)**2))/(a*b)"
 
 # Perhitungan
 mathA = ((a+b)*(b*c))/(a+b+c)
 mathB = P*L*t
-mathC = (1/2)*L*t
-mathD = (((a+b)^2)+((b*c)^2))/(a*b)
+mathC = 1/2*a*t
+mathD = (((a+b)**2)+((b*c)**2))/(a*b)
 
 # Cetak Hasil
 print("Nama  =",nama)
@@ -30,13 +30,13 @@ print("Kelas =",kelas)
 print("Prodi =",prodi)
 print("Tugas =",tugas)
 print("------------------------------------------------")
-print("             Hasil Formula Matematika           ")
+print("             Hasil Rumus Matematika           ")
 print("------------------------------------------------")
 print("Di ketahui :")
 print(" a = P =",a)
 print(" b = L =",b)
 print(" c = t =",c)
-print("________________________________________________")
+print("________________")
 print("Hasil dari Operasi A  :",forA)
 print("                      =",mathA)
 print("Hasil dari Operasi B  :",forB)
