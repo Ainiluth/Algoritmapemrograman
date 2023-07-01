5544da6eebcca1dac729fc57f1b28ff7d0ffde40
Ainiluth <136144828+Ainiluth@users.noreply.github.com>
2023-07-01 13:07:47 +0700

    Create latihan6_tipedatalanjutan.py

---
+++ latihan6_tipedatalanjutan.py
@@ -0,0 +1,33 @@
+# Setting Variabel identitas
+nim   = input("Masukan NIM          :")
+nama  = input("Masukan Nama Lengkap :")
+kelas = input("Masukan Kelas        :")
+prodi = input("Masukan Prodi        :")
+
+# Perhitungan  
+phi = 3.14
+r = int(input("Masukkan panjang jari-jari lingkaran: "))
+
+luas = phi*(r*r)
+keliling = 2*phi*r
+
+#setting variabel
+kal = ["UNNIS","di","belajar","pada","Mahasiswa","Pemrograman","ruang","lab","Algoritma","semester","topik","data","Tipe","dan","dengan","Batang",2]
+
+
+print("----------------------------------------")
+print("            OPERASI ARITMATIKA          ")
+print("----------------------------------------")
+print("     Nim  :%s"%(nim))
+print("     Nama :%s"%(nama))
+print("     Kelas:%s"%(kelas))
+print("     Prodi:%s"%(prodi))
+print("----------------------------------------")
+print("             Lingkaran...               ")
+print("----------------------------------------")
+print("Luas lingkaran adalah :%d"%(luas))
+print("Keliling lingkaran adalah :%d"%(keliling))
+print("----------------------------------------")
+print("             Kalimat...                 ")
+print("----------------------------------------")
+print(kal[4],kal[9],kal[16],kal[2],kal[8],kal[5],kal[14],kal[10],kal[12],kal[11],kal[1],kal[6],"%s"%kal[7],"%s"%kal[1],kal[0],kal[15])
