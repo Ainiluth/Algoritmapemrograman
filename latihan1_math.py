79886a0be2648af15a947861044ee4229cd69453
Ainiluth <136144828+Ainiluth@users.noreply.github.com>
2023-07-02 09:26:12 +0700

    latihan1_math.py

--- latihan1_math.py
+++ latihan1_math.py
@@ -1,61 +1,34 @@
 # Setting Variabel
-
 nim   = "20122079"
-
 nama  = "Luthfia"
-
 kelas = "2A"
-
 prodi = "informatika"
 
 # Setting Variabel nilai 
-
 bhs_indo =76
-
 bhs_ing  =70
-
 pd       =90
-
 mtk      =65
-
 kal1     =67
-
 so       =75
 
 # Perhitungan
-
 rata = (bhs_indo+bhs_ing+pd+mtk+kal1+so)/6
 
 # Cetak Hasil
-
 print("NIM Anda        :",nim)
-
 print("Nama Lengkap    :",nama)
-
 print("Kelas           :",kelas)
-
 print("Program studi   :",prodi)
-
 print("--------------------------------")
-
 print(" Hasil Kartu Hasil Study ")
-
 print("--------------------------------")
-
 print("1.Bahasa Indonesia  =",bhs_indo)
-
 print("2.Bahasa Inggris    =",bhs_ing)
-
 print("3.Pemrograman Dasar =",pd)
-
 print("4.Matematika        =",mtk)
-
 print("5.Kalkulus1         =",kal1)
-
 print("6.Sistem Operasi    =",so)
-
 print("--------------------------------")
-
 print("Rata-rata           =",rata)
-
 print("--------------------------------")
